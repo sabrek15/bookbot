b84ec56f3ee974d60d85114f8979120afbbf4a5e
sabrek15 <reddy.ksb007@gmail.com>
2025-02-20 16:01:13 +0530

    added some file reading code

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+def main():
+    with open(/home/sabrek/workspace/github.com/USERNAME/bookbot/books/frankenstein.txt) as f:
+        file_contents = f.read()
+
+if __name__ == "__main__"
+    main()
